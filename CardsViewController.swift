dfc7eef479e06fa85109fa569a1bdd2b3efb348f
Anton Zuev <anton.zuev@autentia.com>
2019-07-02 17:19:33 +0200

    Changed name of the application

--- CardsViewController.swift
+++ CardsViewController.swift
@@ -23,7 +23,6 @@ class CardsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mainFrame = MainFrame()
-        //self.titleLabel.text = viewModel.getTypeName()
     }
     
     @IBAction func openRestCard(_ sender: Any) {
@@ -33,6 +32,11 @@ class CardsViewController: UIViewController {
     @IBAction func openTooMuchTimeCard(_ sender: Any) {
         mainFrame.showCardViewController(for: self, cardImageName: viewModel.getTooMuchTimeCardImageName(), bottomText: viewModel.getTooMuchTimeCardBottomText())
     }
+    
+    func determineInset() {
+        let top = self.collectionView.bounds.height - self.collectionView.collectionViewLayout.collectionViewContentSize.height
+        self.collectionView.contentInset = UIEdgeInsets(top: top/2, left: 0, bottom: 0, right: 0)
+    }
 }
 
 extension CardsViewController: UICollectionViewDataSource {
@@ -41,6 +45,7 @@ extension CardsViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Card", for: indexPath) as! CardCell
         cell.delegate = self
         cell.cardCellButton.setTitle(viewModel.getName(at: indexPath.row), for: UIControl.State.normal)
@@ -48,6 +53,7 @@ extension CardsViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        self.determineInset()
         let restButtonHeader: RestButtonHeader = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "restButtonHeader", for: indexPath) as! RestButtonHeader
         restButtonHeader.restButton.setTitle("RestButton".localized, for: .normal)
         restButtonHeader.tooMuchTimeButton.setTitle("ContinueButton".localized, for: .normal)
